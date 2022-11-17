342d68c7c0a2b568483edc92582d7b92c30f4936
Van <vanicgame@yandex.ru>
2022-11-17 14:54:33 +0300

    Написание минимального бота

--- main.py
+++ main.py
@@ -2,3 +2,34 @@ import typing
 import discord
 from discord.ext import commands
 import random
+
+# Получение конфигов
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+# Константы
+TOKEN = config["bot_settings"]["token"]
+PREFIX = config["bot_settings"]["prefix"]
+ROLES = {
+	"member" : 854338957436125275,
+	"player" : 854339690290085899,
+	"questionnaire_checker" : 869832826135150612,
+}
+
+# Инициализация бота
+intents = discord.Intents.all()
+intents.members = True
+client = commands.Bot(command_prefix=PREFIX, intents = intents)
+print("START")
+
+# Основной код
+
+# Пинг
+@client.command()
+async def ping(ctx):
+    await ctx.send("pong!")
+
+# Запуск
+client.run(TOKEN)
