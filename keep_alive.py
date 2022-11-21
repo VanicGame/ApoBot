8b202e7c8191086bc54b6d616b6b1eb7a08b29ac
Van <vanicgame@yandex.ru>
2022-11-21 20:40:39 +0300

    Настройка flask

--- keep_alive.py
+++ keep_alive.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return ("Bot is up and running :D")
+	return "Bot is up and running :D"
